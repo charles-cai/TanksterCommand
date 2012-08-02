0e763aa8b5da46e533340f43d788abdc2a542fa7
Nathan Totten <ntotten@microsoft.com>
2012-08-02 12:42:44 -0700

    Moved api controllers to api folder.

---
+++ src/TanksterCommand/Controllers/Api/LeaderboardController.cs
@@ -0,0 +1,20 @@
+﻿namespace TanksterCommand.Controllers.Api
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Script.Serialization;
+    using Models;
+
+    public class LeaderboardController : ApiController
+    {
+        [Queryable(ResultLimit = 10)]
+        public IQueryable<Profile> Get()
+        {
+            TanksterContext context = new TanksterContext();
+            return context.Profiles;
+        }
+    }
+}
